88432c6919b9326322423d4ad91cabc76fd74bae
Yu Chen Hou <houyuchen66@gmail.com>
2014-03-19 23:21:17 -0400

    added schools

--- app/src/main/java/com/icechen1/omnible/app/SchoolValues.java
+++ app/src/main/java/com/icechen1/omnible/app/SchoolValues.java
@@ -11,7 +11,17 @@ public class SchoolValues {
             "John Abott College",
             "Champlain College St-Lambert",
             "Collège de Maisonneuve",
-            "CÉGEP de St-Laurent"};
+            "Cégep de St-Laurent",
+        	"Cégep Édouard-Montpetit",
+        	"Cégep André-Laurendeau",
+        	"Cégep régional de Lanaudière (L'Assomption)",
+        	"Collège de Bois-de-Boulogne",
+        	"Collège Jean-de-Brébeuf",
+        	"Cégep du Vieux Montréal",
+        	"Collège Gérald-Godin",
+			"Champlain College Lennoxville",
+			"Collège Ahuntsic",
+			"Collège André-Grasset"};
     final static String[] urls = new String[] {
             "https://marianopolis.omnivox.ca/Mobl/",
             "https://vaniercollege.omnivox.ca/Mobl/",
@@ -19,5 +29,15 @@ public class SchoolValues {
             "https://johnabbott.omnivox.ca/Mobl/",
             "https://champlaincollege-st-lambert.omnivox.ca/Mobl/",
             "https://cmaisonneuve.omnivox.ca/Mobl/",
-            "https://cegep-st-laurent.omnivox.ca/Mobl/"};
+            "https://cegep-st-laurent.omnivox.ca/Mobl/",
+        	"https://cegepmontpetit.omnivox.ca/Mobl/",
+        	"https://claurendeau.omnivox.ca/Mobl/",
+        	"https://collanaud-assomption.omnivox.ca/Mobl/",
+        	"https://bdeb.omnivox.ca/Mobl/",
+        	"https://brebeuf.omnivox.ca/Mobl/",
+        	"https://cvm.omnivox.ca/Mobl/",
+        	"https://cgodin.omnivox.ca/Mobl/",
+			"https://champlaincollege-lennoxville.omnivox.ca/Mobl/",
+			"https://collegeahuntsic.omnivox.ca/Mobl/‎",
+			"https://grasset.omnivox.ca/Mobl/"};
 }
